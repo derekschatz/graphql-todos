7587c4ba12c7000a32524664ad931829ecca73cb
derekschatz <derekschatz1@gmail.com>
2023-07-19 16:01:59 -0400

    Updates

---
+++ precache-manifest.b0c121e6f6e26c005efada0d4cc906e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "303a06e1e5d6ad25de28",
+    "url": "/derekschatz/react-gql-app.git/static/js/main.303a06e1.chunk.js"
+  },
+  {
+    "revision": "49b969fee301d86d4d26",
+    "url": "/derekschatz/react-gql-app.git/static/js/1.49b969fe.chunk.js"
+  },
+  {
+    "revision": "c8d364e7bf926ece643c",
+    "url": "/derekschatz/react-gql-app.git/static/js/runtime~main.c8d364e7.js"
+  },
+  {
+    "revision": "ec8da160a191fd1f5aa471d194c4aedd",
+    "url": "/derekschatz/react-gql-app.git/index.html"
+  }
+];
